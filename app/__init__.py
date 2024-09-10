dc5350d4b81e5e8c60d3ded1cde10e1f8a6154e7
Felipe Joaquim <felipejoaquimtech@gmail.com>
2024-09-10 20:24:53 -0300

    add requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
-from CONFIG import SECRET_KEY
+from flask_sqlalchemy import SQLAlchemy
+from CONFIG import SECRET_KEY, SQLALCHEMY_URI
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysql+pymysql://'+SECRET_KEY
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = 'mysql+pymysql://'+ SQLALCHEMY_URI
+
+db = SQLAlchemy(app)
+
